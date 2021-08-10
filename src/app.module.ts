1c3268ba634ad278a620813967cec015073a2b07
rmlzy <rmlzy@outlook.com>
2021-08-10 18:43:21 +0800

    feat: include *.entity.js and *.subscriber.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Utils } from '@/core';
 import { config } from './config';
 
-// entities
-import { UserEntity } from './system/user/entities/user.entity';
-import { ArticleEntity } from './demo/article/entities/article.entity';
-import { UserArticleEntity } from './demo/user-article/entities/user-article.entity';
-
-// subscribers
-import { UserSubscriber } from './system/user/entities/user.subscriber';
-
 // modules
 import { AuthModule } from './system/auth/auth.module';
 import { UserModule } from './system/user/user.module';
@@ -36,8 +28,8 @@ import { UserArticleModule } from './demo/user-article/user-article.module';
         username: config.get('MYSQL.USERNAME'),
         password: config.get('MYSQL.PASSWORD') as string,
         database: config.get('MYSQL.DATABASE') as string,
-        subscribers: [UserSubscriber],
-        entities: [UserEntity, ArticleEntity, UserArticleEntity],
+        subscribers: ['dist/**/*.subscriber.js'],
+        entities: ['dist/**/*.entity.js'],
         synchronize: true,
       }),
     }),
